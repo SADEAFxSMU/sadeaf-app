e400315c69da4f67cb49269060ae2939feff51a5
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2020-09-13 13:30:43 +0800

    Nuxt Module for Hasura webhook worker exposed on port 4000 (#52)

---
+++ sadeaf-web/hasura/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pubsub from "./pubsub"
+
+export default {
+  async start(port) {
+    const {publish} = await pubsub()
+
+    const app = express()
+    app.use(bodyParser.json())
+
+    app.post('/_hasura/webhook/:queue', async function (req, res, next) {
+      await publish(req.params.queue, req.body)
+      res.json({})
+    })
+
+    return new Promise((resolve, reject) => {
+      app.listen(port, (err) => {
+        if (err) reject(err)
+        else {
+          console.log(`Hasura webhook listening on: http://localhost:4000/_hasura/webhook`)
+          resolve()
+        }
+      })
+    })
+  }
+}
