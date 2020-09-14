fb50c4aea5c780beff60e9e32e9d375403078459
elihuansen <elihuansen@gmail.com>
2020-09-14 11:08:46 +0800

    Refactor vuex store creation to the beautiful nuxt way

--- sadeaf-web/store/index.js
+++ sadeaf-web/store/index.js
@@ -1,21 +1,25 @@
-import Vuex from 'vuex';
-
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      // Dummy client while auth is under development
-      auth: {
-        client: {
-          id: 1,
-          account: {
-            name: 'Jon',
-            username: 'JonLee1995',
-            email: 'jonlee@gmail.com',
-          }
-        }
+export const state = () => ({
+  /**
+   * Placeholder while nuxt auth + cognito is under development
+   */
+  auth: {
+    userType: 'client',
+    client: {
+      id: 1,
+      account: {
+        name: 'Jon',
+        username: 'JonLee1995',
+        email: 'jonlee@gmail.com',
       }
-    }
-  })
-}
+    },
+  }
+});
 
-export default createStore
+export const mutations = {
+  setUserType(state, {userType}) {
+    state.userType = userType;
+  },
+  setUser(state, {userType, user}) {
+    state.auth = {userType, [userType]: user};
+  },
+}
