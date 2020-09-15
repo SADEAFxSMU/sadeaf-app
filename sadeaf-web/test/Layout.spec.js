19caca02891a6081a1358d98a6a8868b36489dc7
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2020-09-15 23:33:39 +0800

    Stricter tests for layout spec

--- sadeaf-web/test/Layout.spec.js
+++ sadeaf-web/test/Layout.spec.js
@@ -24,21 +24,21 @@ describe('User Role-Specific Navbar', () => {
 
   it('should display correct navbar for "admin" user type', async () => {
     await wrapper.setData({ userType: 'admin' });
-    expect(wrapper.findComponent(AdminNavbar).exists()).toBeTruthy();
+    expect(wrapper.findComponent(AdminNavbar).exists()).toBe(true);
   });
 
   it('should display correct navbar for "client" user type', async () => {
     await wrapper.setData({ userType: 'client' });
-    expect(wrapper.findComponent(ClientNavbar).exists()).toBeTruthy();
+    expect(wrapper.findComponent(ClientNavbar).exists()).toBe(true);
   });
 
   it('should display correct navbar for "volunteer" user type', async () => {
     await wrapper.setData({ userType: 'volunteer' });
-    expect(wrapper.findComponent(VolunteerNavbar).exists()).toBeTruthy();
+    expect(wrapper.findComponent(VolunteerNavbar).exists()).toBe(true);
   });
 
   it('should display correct navbar for "service_requestor" user type', async () => {
     await wrapper.setData({ userType: 'service_requestor' });
-    expect(wrapper.findComponent(ServiceRequestorNavbar).exists()).toBeTruthy();
+    expect(wrapper.findComponent(ServiceRequestorNavbar).exists()).toBe(true);
   });
 });
