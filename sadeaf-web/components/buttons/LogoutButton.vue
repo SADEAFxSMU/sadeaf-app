c319b492fe4e59fb78da7809bf773a87c237007b
elihuansen <elihuansen@gmail.com>
2020-09-29 00:53:53 +0800

    Add logout

---
+++ sadeaf-web/components/buttons/LogoutButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-button @click="logout">
+    Logout
+  </el-button>
+</template>
+
+<script>
+export default {
+  name: "LogoutButton",
+  methods: {
+    logout() {
+      this.$auth.signOut();
+      this.$router.replace('/sign-in');
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
