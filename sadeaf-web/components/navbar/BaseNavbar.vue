3d889cdb8688d65992c9c1318dba9585578e01e6
austinwoon <austinwoon.2017@smu.edu.sg>
2020-10-06 16:23:04 +0800

    commit before checkout

--- sadeaf-web/components/navbar/BaseNavbar.vue
+++ sadeaf-web/components/navbar/BaseNavbar.vue
@@ -10,6 +10,9 @@
       <template slot="title">Account</template>
       <el-menu-item index="/account#profile"> Profile </el-menu-item>
       <el-menu-item index="/account#settings"> Settings </el-menu-item>
+      <el-menu-item
+        v-if="this.$store.state.auth.user.role === 'client' || this.$store.state.auth.user.role === 'admin'"
+        index="/account/blacklist"> Blacklist </el-menu-item>
       <el-menu-item index="/account/notifications"> Notification Preferences </el-menu-item>
     </el-submenu>
     <el-menu-item>
