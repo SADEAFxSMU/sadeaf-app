a4167f972d5ba6010b2fcdccfe7d216e575e0c65
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2020-10-09 00:23:13 +0800

    Implement telegram registration for notifications

---
+++ sadeaf-web/telegram/registration-messages.js
@@ -0,0 +1,15 @@
+export const INVALID_MESSAGE = 'Please send either /start or /register\\.';
+export const START_MESSAGE = `Welcome to the SADEAF Telegram bot\\!
+Please type in /register followed by the email you used to register for the portal\\.
+
+For example,  */register myemail@email\\.com*`;
+export const INVALID_REGISTER_MESSAGE = `
+Please type in /register followed by the email you used to register for the portal\\.
+
+For example,  */register myemail@email\\.com*`;
+export const NOT_REGISTERED_USER = 'You have not yet created an account on the SADEAF portal\\!';
+export const NOTIFICATION_NOT_ENABLED = 'You have not set your notification settings on the SADEAF portal';
+export const TELEGRAM_NOT_ENABLED = 'You have not set Telegram as a notification setting on the SADEAF portal';
+export const USERNAME_MISMATCH = 'Please set the correct username on the SADEAF portal';
+export const UPDATE_CHAT_ID_ERROR = 'Something went wrong\\, please try again soon';
+export const REGISTER_SUCCESS = 'You have been successfully registered';
