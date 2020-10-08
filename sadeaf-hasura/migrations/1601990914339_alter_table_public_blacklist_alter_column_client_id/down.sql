bacb68dbe7119164703a1fa73d10ad21985dbe91
austinwoon <austinwoon.2017@smu.edu.sg>
2020-10-08 23:52:14 +0800

    Squashing migrations

--- sadeaf-hasura/migrations/1601990914339_alter_table_public_blacklist_alter_column_client_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."blacklist" rename column "client_account_id" to "client_id";
--- sadeaf-hasura/migrations/1602060390735_alter_table_public_blacklist_add_unique_client_account_id_volunteer_account_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."blacklist" drop constraint "blacklist_client_account_id_volunteer_account_id_key";
