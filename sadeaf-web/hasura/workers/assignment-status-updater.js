f5fd0563393afdbd6390547208ebf68817bf9802
Eli <31790206+elihuansen@users.noreply.github.com>
2020-10-13 17:53:26 +0800

    Add worker to update assignment statuses via polling (#133)
    * Add AssignmentStatusUpdater worker to update assignments via polling
    
    * Change condition for update to status === "MATCHED"
    
    * Enforce prettier

---
+++ sadeaf-web/hasura/workers/assignment-status-updater.js
@@ -0,0 +1,47 @@
+import { ASSIGNMENT_STATUS_WORKER } from '../../config';
+const { INTERVAL_S } = ASSIGNMENT_STATUS_WORKER;
+import { executeGraphQLQuery } from '../../telegram/hasura-helpers';
+
+const updateAssignmentsMutation = `
+  mutation SetEndedAssignmentsStatusesToComplete($now: timestamp) {
+    update_assignment(
+      where: {
+        _and: [
+          { status: { _eq: "MATCHED" }}
+          { end_dt: { _lte: $now }}
+        ]
+      }
+      _set: {
+        status: "COMPLETE"
+      }
+    ) {
+      returning {
+        id
+      }
+    }
+  }
+`;
+
+async function hasuraUpdateAssignments() {
+  try {
+    const {
+      data: {
+        update_assignment: { returning },
+      },
+    } = await executeGraphQLQuery(updateAssignmentsMutation, 'SetEndedAssignmentsStatusesToComplete', {
+      now: new Date().toISOString(),
+    });
+    return returning;
+  } catch (err) {
+    console.error(`[AssignmentStatusUpdater] ${err}`);
+  }
+}
+
+module.exports = async function () {
+  setInterval(async () => {
+    const result = await hasuraUpdateAssignments();
+    if (result && result.length > 0) {
+      console.log('[AssignmentStatusUpdater] Completed assignments: ' + result.map((assignment) => assignment.id));
+    }
+  }, INTERVAL_S * 1000);
+};
