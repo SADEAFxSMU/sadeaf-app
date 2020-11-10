50d2087d59b43a44022b7d7ec8702a71a9e92e98
elihuansen <elihuansen@gmail.com>
2020-11-10 21:27:12 +0800

    test: Set up cypress for E2E testing

--- sadeaf-web/cypress/integration/auth.js
+++ sadeaf-web/cypress/integration/auth.js
@@ -1,23 +1,20 @@
 const siteUrl = 'http://localhost:3000';
 
-const users = {
-
-}
-
-describe('Typing Test : Input', () => {
-
+describe('Login', () => {
   beforeEach(() => {
     cy.visit(siteUrl);
-  })
+  });
 
   it('should redirect to login page and show login form', () => {
     cy.location('pathname').should('equals', '/sign-in');
     cy.get('amplify-authenticator').should('exist');
-  })
+  });
 
   it('should login as volunteer', { includeShadowDom: true }, () => {
     cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-volunteer@huansen.dev');
     cy.get('[data-test=sign-in-password-input]').first().type('password123');
     cy.get('[data-test="sign-in-sign-in-button"]').first().click();
-  })
+
+    // TODO: Check that volunteer home page loads
+  });
 });
