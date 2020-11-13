d1f16a9e5e17b37d6655283b916a245203394b14
Lee Wei Yuan <35588002+weiyuan95@users.noreply.github.com>
2020-11-13 17:24:33 +0800

    Run script to enable all existing accounts in staging (#176)

--- sadeaf-web/hasura/authz/hasura-helpers.js
+++ sadeaf-web/hasura/authz/hasura-helpers.js
@@ -31,7 +31,26 @@ export async function getHasuraUserIdAndRole(user) {
   return { id, role };
 }
 
-function hasuraRoleAndIdQuery(cognitoId) {
+async function hasuraRoleAndIdQuery(cognitoId) {
+  // Temporary script to change all account is_enabled statuses to true
+  const query =`mutation MyMutation {
+    update_account(_set: {is_enabled: true}, where: {}) {
+      affected_rows
+    }
+  }`;
+
+  // execute temp script
+  await fetch(HASURA.GRAPHQL_API_URL, {
+    headers: {
+      'X-Hasura-Admin-Secret': HASURA.GRAPHQL_ADMIN_SECRET,
+    },
+    body: JSON.stringify({
+      query,
+      operationName: 'MyMutation',
+    }),
+    method: 'POST',
+  });
+
   return fetch(HASURA.GRAPHQL_API_URL, {
     headers: {
       'X-Hasura-Admin-Secret': HASURA.GRAPHQL_ADMIN_SECRET,
