c6f540cbff87c4aaa1e026c178c83179d3518070
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2020-11-16 21:21:37 +0800

    Make login tests less flaky - Shadowdom is now default setting for cypress

--- sadeaf-web/cypress/integration/auth.js
+++ sadeaf-web/cypress/integration/auth.js
@@ -10,30 +10,30 @@ describe('Login', () => {
     cy.get('amplify-authenticator').should('exist');
   });
 
-  it('should show /registration for non-enabled users', { includeShadowDom: true }, () => {
-    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-user@huansen.dev');
-    cy.get('[data-test=sign-in-password-input]').first().type('password123');
+  it('should show /registration for non-enabled users', () => {
+    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-user@huansen.dev', { force: true });
+    cy.get('[data-test=sign-in-password-input]').first().type('password123', { force: true });
     cy.get('[data-test="sign-in-sign-in-button"]').first().click();
     cy.location('pathname').should('equals', '/registration');
   });
 
-  it('should show /client for clients', { includeShadowDom: true }, () => {
-    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-client@huansen.dev');
-    cy.get('[data-test=sign-in-password-input]').first().type('password123');
+  it('should show /client for clients', () => {
+    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-client@huansen.dev', { force: true });
+    cy.get('[data-test=sign-in-password-input]').first().type('password123', { force: true });
     cy.get('[data-test="sign-in-sign-in-button"]').first().click();
     cy.location('pathname').should('equals', '/client');
   });
 
-  it('should show /admin for admin', { includeShadowDom: true }, () => {
-    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-admin@huansen.dev');
-    cy.get('[data-test=sign-in-password-input]').first().type('password123');
+  it('should show /admin for admin', () => {
+    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-admin@huansen.dev', { force: true });
+    cy.get('[data-test=sign-in-password-input]').first().type('password123', { force: true });
     cy.get('[data-test="sign-in-sign-in-button"]').first().click();
     cy.location('pathname').should('equals', '/admin');
   });
 
-  it('should show /volunteer for clients', { includeShadowDom: true }, () => {
-    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-volunteer@huansen.dev');
-    cy.get('[data-test=sign-in-password-input]').first().type('password123');
+  it('should show /volunteer for volunteer', () => {
+    cy.get('[data-test=sign-in-email-input]').first().type('sadeaf-volunteer@huansen.dev', { force: true });
+    cy.get('[data-test=sign-in-password-input]').first().type('password123', { force: true });
     cy.get('[data-test="sign-in-sign-in-button"]').first().click();
     cy.location('pathname').should('equals', '/volunteer');
   });
